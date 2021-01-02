4f0802ca756af8c66baa35496a402d8e55c26b4a
migger <i.mihailyuk@cian.ru>
2021-01-02 16:30:01 +0300

    initial

--- index.js
+++ index.js
@@ -80,4 +80,42 @@ module.exports = class {
             return response.data;
         });
     }
+    async createBucket(bucket) {
+        if(this.options.verbose) {
+            console.log({bucket});
+        }
+        return axios.put(`https://${bucket}.storage.yandexcloud.net/`, {
+            validateStatus: (status) => {
+                return true
+            }
+        }).then(async (response) => {
+            if(response.status > 299) {
+                console.log(response.status, "\n", response.data);
+                throw new Error('Error while sending request');
+            }
+            if(this.options.verbose) {
+                console.log(response.status, "\n", response.data);
+            }
+            return response.data;
+        });
+    }
+    async deleteBucket(bucket) {
+        if(this.options.verbose) {
+            console.log({bucket, targetPath});
+        }
+        return axios.delete(`https://${bucket}.storage.yandexcloud.net/`, {
+            validateStatus: (status) => {
+                return true
+            }
+        }).then(async (response) => {
+            if(response.status > 299) {
+                console.log(response.status, "\n", response.data);
+                throw new Error('Error while sending request');
+            }
+            if(this.options.verbose) {
+                console.log(response.status, "\n", response.data);
+            }
+            return response.data;
+        });
+    }
 }
